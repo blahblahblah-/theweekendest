7b5bc2e791e779bc6b3baaa100269324311290c3
Sunny Ng <sunny@harrys.com>
2018-10-06 17:14:31 -0400

    Rought version

--- src/app/index.js
+++ src/app/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import '../style/app.scss';
+import '../vendors/leaflet.polylineoffset.js'
 import App from './app';
 
 document.addEventListener('DOMContentLoaded', () => {
--- src/app/map_container.js
+++ src/app/map_container.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Map, Marker, Popup, TileLayer, CircleMarker, Polyline } from 'react-leaflet';
+import L from 'leaflet';
 import stationData from '../data/stations.json';
 
 const bounds = [[40.512764,-74.251961], [40.903125,-73.755405]]
-const apiUrl = 'http://localhost:3000/api/routes';
+const apiUrl = 'https://www.goodservice.io/api/routes';
 const stations = {};
+const center = [40.7079445, -74.003683]
 
 class MapContainer extends React.Component {
   constructor(props) {
@@ -18,6 +19,23 @@ class MapContainer extends React.Component {
   componentDidMount() {
     this.fetchData();
     this.timer = setInterval(() => this.fetchData(), 60000);
+    // create map
+    this.map = L.map('map', {
+      center: center,
+      maxBounds: bounds,
+      zoom: 11,
+      layers: [
+        L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.png', {
+          attribution: 'Map tiles by <a href=&quot;http://stamen.com&quot;>Stamen Design</a>,\
+            under <a href=&quot;http://creativecommons.org/licenses/by/3.0&quot;>CC BY 3.0</a>.\
+            Data by <a href=&quot;http://openstreetmap.org&quot;>OpenStreetMap</a>,\
+            under <a href=&quot;http://www.openstreetmap.org/copyright&quot;>ODbL</a>.'
+        }),
+      ]
+    });
+    this.linesLayer = L.layerGroup().addTo(this.map);
+    this.stationsLayer = L.layerGroup().addTo(this.map);
+    this.renderStations();
   }
 
   componentWillUnmount() {
@@ -27,22 +45,19 @@ class MapContainer extends React.Component {
   fetchData() {
     fetch(apiUrl)
       .then(response => response.json())
-      .then(data => this.setState({routes: data.routes}))
+      .then(data => this.renderLines(data.routes))
   }
 
   renderStations() {
-    return Object.values(stations).map(
-      (station) => {
-        return (
-          <CircleMarker center={[station.latitude, station.longitude]} radius={2} color='#000000' key={station.stopId} />
-        )
-      }
+    this.stationsLayer.clearLayers();
+    Object.values(stations).forEach((station) =>
+      L.circleMarker([station.latitude, station.longitude], { title: station.name, radius: 2, color: "#000000" }).addTo(this.stationsLayer)
     )
   }
 
-  renderLines() {
-    const {routes} = this.state;
-    return Object.keys(routes).map(
+  renderLines(routes) {
+    this.linesLayer.clearLayers();
+    return Object.keys(routes).forEach(
       (key, index) => {
         const route = routes[key]
         const north = route.routings.north.map(
@@ -53,28 +68,15 @@ class MapContainer extends React.Component {
                 return [station.latitude, station.longitude];
               }
             );
-            return (
-              <Polyline positions={positions} color={route.color} opacity={0.5} offset={index * 10} key={route.name + "-" + routing[0] && routing[0].stopId}>
-                <Popup>
-                  <h3>{route.name} - Uptown</h3>
-                  <ul>
-                    {
-                      routing.map(
-                        (stopId) => {
-                          const station = stations[stopId.substr(0, 3)];
-                          return (
-                            <li key={stopId}>station.name</li>
-                          )
-                        }
-                      )
-                    }
-                  </ul>
-                </Popup>
-              </Polyline>
-            )
+            const line = L.polyline(positions, {color: route.color, opacity: 0.3}).addTo(this.map);
+
+            // const odd = (index % 2) ? -1 : 1;
+            // const offset = (index / 2) * odd;
+            // console.log(offset);
+            // line.setOffset(offset);
           }
         )
-        const south = route.routings.south.map(
+        const south = route.routings.south.forEach(
           (routing) => {
             const positions = routing.map(
               (stopId) => {
@@ -82,49 +84,21 @@ class MapContainer extends React.Component {
                 return [station.latitude, station.longitude];
               }
             );
-            return (
-              <Polyline positions={positions} color={route.color} opacity={0.5} offset={index * 10} key={route.name + "-" + routing[0] && routing[0].stopId}>
-                <Popup>
-                  <h3>{route.name} - Downtown</h3>
-                  <ul>
-                    {
-                      routing.map(
-                        (stopId) => {
-                          const station = stations[stopId.substr(0, 3)];
-                          return (
-                            <li key={stopId}>{station.name}</li>
-                          )
-                        }
-                      )
-                    }
-                  </ul>
-                </Popup>
-              </Polyline>
-            )
+            const line = L.polyline(positions, {color: route.color, opacity: 0.3}).addTo(this.map);
+
+            // const odd = (index % 2) ? -1 : 1;
+            // const offset = (index / 2) * odd;
+            // console.log(offset);
+            // line.setOffset(offset);
           }
         )
-        return north.concat(south);
       }
     )
   }
 
   render() {
     return (
-      <Map bounds={bounds} maxBounds={bounds} minZoom={11} style={{height: 700}}>
-        {
-          this.renderStations()
-        }
-        {
-          this.renderLines()
-        }
-        <TileLayer
-          attribution="Map tiles by <a href=&quot;http://stamen.com&quot;>Stamen Design</a>,
-            under <a href=&quot;http://creativecommons.org/licenses/by/3.0&quot;>CC BY 3.0</a>.
-            Data by <a href=&quot;http://openstreetmap.org&quot;>OpenStreetMap</a>,
-            under <a href=&quot;http://www.openstreetmap.org/copyright&quot;>ODbL</a>."
-          url="https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.png"
-        />
-      </Map>
+      <div id="map" style={{height: "700px"}}></div>
     )
   }
 }
