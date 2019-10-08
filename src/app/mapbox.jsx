56dc24c6548fecc38644e153f950ccfe1e1ee3fa
Sunny Ng <sunny@sunny-ng.com>
2019-10-07 21:27:38 -0400

    Added stop symbols

--- src/app/mapbox.jsx
+++ src/app/mapbox.jsx
@@ -4,7 +4,6 @@ import mapboxgl from 'mapbox-gl';
 import { Header, Segment, Grid, Button, Icon } from "semantic-ui-react";
 import stationData from '../data/station_details.json';
 
-const bounds = [[-74.3586, 41.2176], [-73.5183, 40.215]]
 const apiUrl = 'https://www.goodservice.io/api/routes';
 const stations = {};
 const center = [-74.003683, 40.7079445]
@@ -17,8 +16,10 @@ class Mapbox extends React.Component {
     this.state = {routes: {}};
     Object.keys(stationData).forEach((key) => {
       stations[key] = stationData[key];
-      stations[key]["northStops"] = [];
-      stations[key]["southStops"] = [];
+      stations[key]["northStops"] = new Set();
+      stations[key]["southStops"] = new Set();
+      stations[key]["passed"] = new Set();
+      stations[key]["stops"] = new Set();
     });
   }
   
@@ -27,7 +28,6 @@ class Mapbox extends React.Component {
       container: this.mapContainer,
       style: 'mapbox://styles/theweekendest/ck1fhati848311cp6ezdzj5cm',
       center: center,
-      // maxBounds: bounds,
       bearing: 29,
       minZoom: 9,
       zoom: 12,
@@ -45,56 +45,14 @@ class Mapbox extends React.Component {
     this.map.on('load', () => {
       this.fetchData();
       this.timer = setInterval(() => this.fetchData(), 360000);
-      this.map.addLayer({
-        "id": "Stops",
-        "type": "symbol",
-        "source": {
-          "type": "geojson",
-          "data": this.stopsGeoJson()
-        },
-        "layout": {
-          "text-field": ['get', 'name'],
-          "text-size": {
-            "stops": [[8, 12], [15, 14]]
-          },
-          "text-anchor": "right",
-          "text-optional": true,
-          "text-justify": "left",
-          "text-padding": 10,
-          // "text-variable-anchor": ['bottom-right', 'right', 'top-right', 'bottom', 'bottom-left', 'left', 'top-left']
-        },
-        "paint": {
-          "text-translate": {
-            "stops": [[8, [-5, 0]], [14, [-12, -15]]]
-          },
-          "text-color": "#aaaaaa"
-        }
-      })
     });
-  }
-
-  stopsGeoJson() {
-    return {
-      "type": "FeatureCollection",
-      "features": Object.keys(stations).map((key) => {
-        return {
-          "type": "Feature",
-          "properties": {
-            "name": stations[key].name.replace(/ - /g, "–")
-          },
-          "geometry": {
-            "type": "Point",
-            "coordinates": [stations[key].longitude, stations[key].latitude]
-          }
-        }
-      })
-    };
   }
 
   fetchData() {
     fetch(apiUrl)
       .then(response => response.json())
       .then(data => this.renderLines(data.routes))
+      .then(() => this.renderStops())
   }
 
   renderLines(routes) {
@@ -102,6 +60,7 @@ class Mapbox extends React.Component {
       stations[key] = stationData[key];
       stations[key]["northStops"] = new Set();
       stations[key]["southStops"] = new Set();
+      stations[key]["passed"] = new Set();
       stations[key]["stops"] = new Set();
     });
 
@@ -109,6 +68,8 @@ class Mapbox extends React.Component {
     const routeStops = {}
 
     Object.keys(routes).forEach((key) => {
+      const northStops = new Set();
+      const southStops = new Set();
       const route = routes[key];
       const layerId = `${key}-train`;
       routeStops[key] = new Set();
@@ -122,6 +83,7 @@ class Mapbox extends React.Component {
           stations[stopIdPrefix].northStops.add(key);
           stations[stopIdPrefix].stops.add(key);
           routeStops[key].add(stopIdPrefix);
+          northStops.add(stopIdPrefix);
           return stopIdPrefix;
         });
       });
@@ -135,6 +97,7 @@ class Mapbox extends React.Component {
           stations[stopIdPrefix].southStops.add(key);
           stations[stopIdPrefix].stops.add(key);
           routeStops[key].add(stopIdPrefix);
+          southStops.add(stopIdPrefix);
           return stopIdPrefix;
         }).reverse();
       });
@@ -144,9 +107,10 @@ class Mapbox extends React.Component {
       const geojson = {
         "type": "FeatureCollection",
         "features": routings.map((routing) => {
-          return this.routingGeoJson(routing)
+          return this.routingGeoJson(key, routing)
         })
       };
+      
       routeLayers[layerId] = {
         "id": layerId,
         "type": "line",
@@ -168,10 +132,10 @@ class Mapbox extends React.Component {
     });
 
     const offsets = {};
-    const offsetMap = [[[8, 0], [14, 0], [16, 0]], [[8, -3], [14, -5], [16, -7]], [[8, 3], [14, 5], [16, 7]], [[8, -6], [14, -10], [16, -14]], [[8, 6], [14, 10], [16, 14]], [[8, -9], [14, -15], [16, -21]], [[8, 9], [14, 15], [16, 21]]];
+    const offsetMap = [[[8, 0], [14, 0], [16, 0]], [[8, -2], [14, -4], [16, -6]], [[8, 2], [14, 4], [16, 6]], [[8, -4], [14, -8], [16, -12]], [[8, 4], [14, 8], [16, 12]], [[8, -6], [14, -12], [16, -18]], [[8, 6], [14, 12], [16, 18]]];
     const textOffsetMap = [[0, 0], [-1, 0], [1, 0], [-2, 0], [2, 0], [-3, 0], [3, 0]];
 
-    ['2', '3', '1', '4', '5', '6', '7', '7X', 'A', 'C', 'E', 'F', 'FX', 'D', 'B', 'M', 'J', 'Z', 'N', 'Q', 'R', 'W', 'G', 'H', 'FS', 'GS'].forEach((train) => {
+    ['2', '3', '1', '4', '5', '6', '7', '7X', 'A', 'C', 'E', 'F', 'FX', 'D', 'B', 'M', 'J', 'Z', 'N', 'Q', 'R', 'W', 'G', 'H', 'FS', 'GS', "L", "SI"].forEach((train) => {
       const layerId = `${train}-train`;
       const routeLayer = routeLayers[layerId];
 
@@ -200,32 +164,16 @@ class Mapbox extends React.Component {
           this.map.removeLayer(layerId)
         }
         this.map.addLayer(routeLayer);
-        // this.map.addLayer({
-        //   "id": `${layerId}-symbol`,
-        //   "type": "symbol",
-        //   "source": {
-        //     "type": "geojson",
-        //     "data": routeLayer.source.data,
-        //   },
-        //   "layout": {
-        //     "symbol-placement": "line",
-        //     "text-font": ["Open Sans Regular"],
-        //     "text-field": train,
-        //     "text-size": 14,
-        //     "text-rotation-alignment": "viewport",
-        //     "text-offset": textOffsetMap[offset]
-        //   },
-        // });
       }
     });
   }
 
-  routingGeoJson(routing) {
+  routingGeoJson(train, routing) {
     let path = []
     let prev = routing.splice(0, 1);
     routing.forEach((stopId) => {
       path.push([stations[prev].longitude, stations[prev].latitude]);
-      let potentialPath = this.findPath(prev, stopId, 0);
+      let potentialPath = this.findPath(prev, stopId, 0, train);
       if (potentialPath) {
         potentialPath.forEach((coord) => {
           path.push(coord);
@@ -247,7 +195,7 @@ class Mapbox extends React.Component {
     }
   }
 
-  findPath(start, end, stepsTaken) {
+  findPath(start, end, stepsTaken, train) {
     if (stations[start]["north"][end] != undefined) {
       if (stations[start]["north"][end].length) {
         return stations[start]["north"][end];
@@ -258,8 +206,9 @@ class Mapbox extends React.Component {
     }
     let results = [];
     Object.keys(stations[start]["north"]).forEach((key) => {
-      const path = this.findPath(key, end, stepsTaken + 1);
+      const path = this.findPath(key, end, stepsTaken + 1, train);
       if (path && path.length) {
+        stations[start]["passed"].add(train);
         if (stations[start]["north"][key].length) {
           return results = stations[start]["north"][key].concat(path);
         }
@@ -267,6 +216,58 @@ class Mapbox extends React.Component {
       }
     });
     return results;
+  }
+
+  renderStops() {
+    this.map.addLayer({
+      "id": "Stops",
+      "type": "symbol",
+      "source": {
+        "type": "geojson",
+        "data": this.stopsGeoJson()
+      },
+      "layout": {
+        "text-field": ['get', 'name'],
+        "text-size": {
+          "stops": [[8, 10], [13, 14]]
+        },
+        "text-anchor": "right",
+        "text-optional": true,
+        "text-justify": "left",
+        "text-padding": 10,
+        "icon-image": ['get', 'stopType'],
+        "icon-size": {
+          "stops": [[8, 0.25], [12, 0.75], [14, 1]]
+        },
+        "icon-allow-overlap": true,
+        // "text-variable-anchor": ['bottom-right', 'right', 'top-right', 'bottom', 'bottom-left', 'left', 'top-left']
+      },
+      "paint": {
+        "text-translate": {
+          "stops": [[8, [-5, 0]], [14, [-12, -15]]]
+        },
+        "text-color": "#aaaaaa"
+      }
+    });
+  }
+
+  stopsGeoJson() {
+    return {
+      "type": "FeatureCollection",
+      "features": Object.keys(stations).map((key) => {
+        return {
+          "type": "Feature",
+          "properties": {
+            "name": stations[key].name.replace(/ - /g, "–"),
+            "stopType": "circle-15"
+          },
+          "geometry": {
+            "type": "Point",
+            "coordinates": [stations[key].longitude, stations[key].latitude]
+          }
+        }
+      })
+    };
   }
 
   render() {
