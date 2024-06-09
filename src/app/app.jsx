c91641b484980cb0b539b272eb2b8de67871de3e
Sunny Ng <sunny@sunny-ng.com>
2024-06-09 03:08:19 -0400

    Fix copy

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -20,7 +20,7 @@ class App extends React.Component {
         <Message color='black' className='toast'>
           <Message.Header>Save Congestion Pricing!</Message.Header>
           <p>
-            Contact your local elected officials, and let them that know Gov. Kathy Hochul should not be allowed to derail MTA's $15 billion capital plan.&nbsp;
+            Contact your local electeds, and let them know Gov. Kathy Hochul should not derail MTA's $15 billion capital plan.&nbsp;
             Click <a href="https://savecongestionpricing.org" onClick={() => this.trackOutboundLink('https://savecongestionpricing.org')} target="_blank">here</a> to find out how.
           </p>
         </Message>
