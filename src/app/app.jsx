821bd9d9e22ff862a3eefdcd3530ee579a4f122f
Sunny Ng <sunny@sunny-ng.com>
2024-07-06 19:43:22 -0400

    Revert "Save congestion pricing"
    This reverts commit c7169d6c8688a8e4478addbffb862b6a551c0199.

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Mapbox from './mapbox.jsx';
-import { Message } from "semantic-ui-react";
 
 class App extends React.Component {
   trackOutboundLink(url) {
@@ -17,13 +16,6 @@ class App extends React.Component {
   render() {
     return(
       <Router>
-        <Message color='black' className='toast'>
-          <Message.Header>Save Congestion Pricing!</Message.Header>
-          <p>
-            Contact your local elected officials, and let them that know Gov. Kathy Hochul should not be allowed to derail MTA's $15 billion capital plan.&nbsp;
-            Click <a href="https://savecongestionpricing.org" onClick={() => this.trackOutboundLink('https://savecongestionpricing.org')} target="_blank">here</a> to find out how.
-          </p>
-        </Message>
         <Mapbox />
       </Router>
     )
